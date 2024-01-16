c74090afdb8264ad5abd3ed8341fbee0a1dc7697
Zhen Marie Villanueva <2022zmgvillanueva@live.mcl.edu.ph>
2024-01-16 12:00:54 +0800

    done task number 2

--- 2. Control Structures/index.js
+++ 2. Control Structures/index.js
@@ -17,6 +17,7 @@ if (randomNumber % 2 === 0) {
     console.log("The number is odd");
   }
 
+
 // TODO 2.3 If the number if a multiple of 3, write "fizz".
 // if the number is a multiple of 5 display "buzz".
 // if the number is divisible by both 3 and 5, display "fizzbuzz". otherwise, display the number
@@ -39,41 +40,60 @@ console.log("toDisplay", toDisplay);
 based on the value of a single expression, making it useful when you have multiple possible values to compare against. */
 
 // TODO 2.5 Use a for loop to print the numbers 1 to N
-const n = 10
+const n = 15
 for (let i = 1; i <= n; i++) {
     console.log(i);
   }
 
 const list = ["apple", "banana", "cherry", "date", "elderberry"];
 // TODO 2.6 Use a while loop to display all the values in the list
-// Your code here
+let i = 0;
+
+while (i < list.length) {
+  console.log(list[i]);
+  i++;
+}
 
 // Checkpoint 2.3 What is the difference between do while and while loop?
-// Answer: 
+/* Answer: The main difference between a while loop and a do-while loop is that a while loop checks the
+condition before executing the loop, while a do-while loop checks the condition after executing the loop. */
 
 // TODO 2.7 Use a for of loop to display all the values in the list
-// Your code here
+for (const value of list) {
+  console.log(value);
+}
 
 // TODO 2.8 Use a for in loop to display all the values in the list
-// Your code here
+for (const index in list) {
+    console.log(list[index]);
+  }
 
 // TODO 2.9 Use the for each method of the list to display all its values 
-// Your code here
+list.forEach(value => {
+    console.log(value);
+  });
 
 // Checkpoint 2.2 When should you use for of, for in, or .forEach loops? Try researching the answer
-// Answer: 
+/* Answer: The for of loop is used to iterate over the values of an iterable object, such as an array or a string, and execute a block of code for each value.
+The for in loop is used to iterate over the properties of an object and execute a block of code for each property.
+The .forEach loop is a method available for arrays in JavaScript and is used to iterate over each element of the array
+and execute a callback function for each element. */
 
 // TODO 2.10 Use the try and catch block to catch division by zero errors in the code below.
 // In the finally block, simulate cleaning up resources by displaying "cleaning up resources"
 const numerator = Math.floor((Math.random() * 100) + 1);
 const denominator = Math.floor((Math.random() * 5)); // feel free to mock this value for testing
 
-
-if(denominator === 0){
-    throw new Error("Division by zero error")
-} else {
-    console.log(numerator / denominator);
+try {
+    if (denominator === 0) {
+        throw new Error("Division by zero error");
+    } else {
+        console.log(numerator / denominator);
+    }
+} catch (error) {
+    console.error(error);
+} finally {
+    console.log("Cleaning up resources");
 }
-
 
 
